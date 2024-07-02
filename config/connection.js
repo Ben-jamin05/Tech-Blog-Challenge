beec8396d974035b4aa599b05885be0e53ec66f8
Ben Hill <benjaminjhill@icloud.com>
2024-07-01 21:22:36 -0500

    created folders and some files

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: '127.0.0.1',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
