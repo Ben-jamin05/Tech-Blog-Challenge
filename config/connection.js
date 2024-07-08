16ce6e0d1d94936cc62fda60cca6d5be13fc4c8c
Ben Hill <benjaminjhill@icloud.com>
2024-07-08 12:02:19 -0500

    more render trying

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: process.env.DB_URL || '127.0.0.1',
+      host: '127.0.0.1',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
