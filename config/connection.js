452828ebaa71489877dc9694f455a6adda8d8af2
Ben Hill <benjaminjhill@icloud.com>
2024-07-08 11:57:28 -0500

    trying to fix render issues

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: '127.0.0.1',
+      host: process.env.DB_URL || '127.0.0.1',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
